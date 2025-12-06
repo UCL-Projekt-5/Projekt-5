eee71c03a0dc24405c3231dcbfc9b30cdb2df70f
JosefineSvarre <josefinesvarre@hotmail.com>
2025-12-06 14:44:06 +0100

    Update margin for section and add customer reviews data array

--- js/anmeldelser.js
+++ js/anmeldelser.js
@@ -2,3 +2,31 @@
 //        KUNDEANMELDELSER KARRUSEL           //
 //                Josefine                    //
 // ------------------------------------------ //
+
+//  Array med anmeldelser (objekter)
+const reviews = [
+	{
+		text: "Vi havde en helt fantastisk weekend hos Hesselgaard! Teltene er super hyggelige, omgivelserne smukke, og der er plads til både ro og sjov. Børnene elskede naturen omkring glampingstedet, og vi voksne kunne slappe helt af. Vi glæder os allerede til næste gang!",
+		name: "Pernille D.",
+		rating: 5,
+		time: "3 måneder siden",
+	},
+	{
+		text: "Et fuldstændig magisk sted. Stilheden, naturen og stemningen var helt unik.",
+		name: "Mikkel R.",
+		rating: 5,
+		time: "1 måned siden",
+	},
+	{
+		text: "Perfekt getaway! Børnene elskede dyrene og aktiviteterne! Vi kommer helt sikkert igen.",
+		name: "Camilla S.",
+		rating: 4,
+		time: "2 uger siden",
+	},
+	{
+		text: "God oplevelse, men lidt langt fra stranden.",
+		name: "Jonas T.",
+		rating: 3,
+		time: "4 dage siden",
+	},
+];
