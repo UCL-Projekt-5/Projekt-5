b2b2d53420c05e5f333972fe3e6a8482829cbba7
JosefineSvarre <josefinesvarre@hotmail.com>
2025-12-06 16:47:44 +0100

    Add navigation buttons for customer reviews and update review display logic

--- js/anmeldelser.js
+++ js/anmeldelser.js
@@ -4,6 +4,8 @@
 // ------------------------------------------ //
 
 //  Array med anmeldelser (objekter)
+// Et array er en liste.
+// Hver anmeldelse er et objekt med tekst, navn og tidspunkt.
 const reviews = [
 	{
 		text: "Vi havde en helt fantastisk weekend hos Bibi! Teltene var hyggelige og klang var spot on!",
@@ -28,6 +30,41 @@ const reviews = [
 ];
 
 // 2. Variabel til at holde styr på hvilken anmeldelse der vises
+// currentIndex fortæller hvilket nummer i arrayet vi er på.
+// 0 = første anmeldelse, 1 = anden anmeldelse osv.
 let currentIndex = 0;
 
 // 3. Henter elementer fra DOM
+const reviewBox = document.getElementById("reviewBox");
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
+
+// 4. eventlisteners til knapperne
+// Når man klikker på højre pil, skal næste anmeldelse vises.
+// Når man klikker på venstre pil, skal forrige anmeldelse vises.
+btnRight.addEventListener("click", nextReview);
+btnLeft.addEventListener("click", previousReview);
+
+// 5. Funktion til at vise den næste anmeldelse.
+function nextReview() {
+	currentIndex++; // Her lægger vi 1 til currentIndex, så vi går én anmeldese frem.
+	if (currentIndex >= reviews.length) {
+		// Større eller lig med 4
+		currentIndex = 0;
+	}
+	updateReviewBox();
+}
+
+// 6. Funktion til at vise den forrige anmeldelse.
+function previousReview() {
+	currentIndex--; // Her trækker vi 1 fra currentIndex, så vi går én anmeldese tilbae.
+	if (currentIndex < 0) {
+		currentIndex = 3;
+	}
+	updateReviewBox();
+}
+
+// 7. Her laver vi en funktion, der opdaterer indholdet i reviewBox
+function updateReviewBox() {
+	const review = reviews[currentIndex];
+}
