f42181a1b0b4ed19dd90e254eaca39ac1b6ee3d7
CupcakeFlip <70489875+CupcakeFlip@users.noreply.github.com>
2025-12-10 22:10:25 +0100

    Fjernet min todo

--- js/booking.js
+++ js/booking.js
@@ -1,14 +1,3 @@
-/**
- * Todo
- * * Slut af med at have et booking system der kan udregne prisen for ens ophold og indsætte det på siden.
- * * Når man trykker på et telt så bliver det valgt. active class bliver tilføjet til teltets bookTentContainer
- * * Data der skal indsamles
- * Antal gæster, hvis mere end 4 så + 300 kr per mængde over 4
- * Ankomst og afrejse, udregne pris i alt, også tag i mende om det er lav sæson eller høj sæson
- * Ekstra tilkøb
- *
- */
-
 const form = document.getElementById("bookingForm");
 const displayPrice = document.getElementById("displayPrice");
 const tentContainers = document.getElementsByClassName("bookTentContainer");
